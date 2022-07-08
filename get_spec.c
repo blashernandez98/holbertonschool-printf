2027b877a6309b48f902b3b495a9a18dff7b8b21
Blas <blashernandez98@gmail.com>
2022-07-08 10:40:19 -0700

    task0

--- get_spec.c
+++ get_spec.c
@@ -1,25 +1,31 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 /**
-* get_spec - map a char with a function
-* @s: char
-* Return: NULL or a pointer function
-*/
+ * get_spec - Get correct function to print.
+ * @c: Specifier char.
+ *
+ * Return: Pointer to function that prints.
+ */
 
-int (*get_spec(char s))(va_list)
+int (*get_spec(char c))(va_list)
 {
-	spec_t specs[] = {
+	int i;
+	spec_t spec_arr[] = {
 		{'c', f_ch},
 		{'s', f_str},
-		{'%', f_percent}
+		{'%', f_percent},
+		{'\0', f_nil},
+		{'\n', f_nl},
+		{'d', f_int},
+		{'i', f_int}
 	};
-	int i;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 7; i++)
 	{
-		if (s == specs[i].spec)
-			return (specs[i].f);
+		if (c == spec_arr[i].spec)
+			return (spec_arr[i].f);
 	}
 	return (NULL);
 }
