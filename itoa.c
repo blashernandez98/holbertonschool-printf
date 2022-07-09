8c74deb346aa2e14cf488ea5d40ac908f32f0ec6
Blas <blashernandez98@gmail.com>
2022-07-09 10:37:15 -0700

    task3

--- itoa.c
+++ itoa.c
@@ -8,22 +8,17 @@
  * Return: Number of digits printed.
  */
 
-int itoa(unsigned int num, unsigned int base)
+int itoa(unsigned int num, unsigned int base, char *digits)
 {
 	int res = 0;
 	long int n = num;
 
-	if (n < 0)
-	{
-		n *= -1;
-		res += _putchar('-');
-	}
 	if (n / base != 0)
 	{
-		res += itoa(n / base, base);
-		res += _putchar(n % base + '0');
+		res += itoa(n / base, base, digits);
+		res += _putchar(digits[n % base]);
 	}
 	else
-		return (res + _putchar(n + '0'));
-	return (res);
+		return (res + _putchar(digits[n % base]));
+
 }
--- print_ints.c
+++ print_ints.c
@@ -10,7 +10,16 @@
 
 int f_int(va_list arg_ptr)
 {
-	return (itoa(va_arg(arg_ptr, int), 10));
+	long int n = va_arg(arg_ptr, int);
+	int res = 0;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+		res++;
+	}
+	return (itoa(n, 10, "0123456789") + res);
 }
 
 /**
@@ -23,5 +32,5 @@ int f_int(va_list arg_ptr)
 int f_bin(va_list arg_ptr)
 {
 	unsigned int num = va_arg(arg_ptr, unsigned int);
-	return (itoa(num, 2));
+	return (itoa(num, 2, "01"));
 }
